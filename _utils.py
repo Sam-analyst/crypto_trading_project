56cd9d5df751bedaafa8ca245c39e7f463c084e2
Dev <dev@Devs-MacBook-Air.local>
2023-03-16 21:43:25 -0400

    fixed all issues related to timezones

--- _utils.py
+++ _utils.py
@@ -1,4 +1,6 @@
 import yaml
+from datetime import datetime
+import pytz
 
 def read_config() -> dict:
     '''
@@ -96,3 +98,56 @@ def get_candlestick_url(exchange: str):
     return candlestick_url
 
 
+def get_datetime(date: str,
+                 time: str,
+                 date_format: str = '%Y-%m-%d',
+                 time_format: str = '%H:%M:%S',
+                 convert_to_utc: bool = False,
+                 local_timezone: str = None,
+                 ) -> datetime:
+    '''
+    A function that takes a date and a time and converts it to a datetime
+    object. This function also converts to UTC datetime if needed,
+    but local_timezone will also need to be given.
+
+    Parameters
+    ----------
+    date: str
+        The date to be converted to datetime. The given date should
+        have the format 'YYYY-MM-DD', unless specified otherwise by
+        providing a different date_format.
+    time: str
+        The time to be converted to datetime. The given time should
+        have the format 'HH:MM:DD', unless specified otherwise by
+        providing a different time_format.
+    date_format: str
+        The format code of the date provided. The available format codes 
+        can be referenced in the datetime module documentation.
+    time_format: str
+        The format code of the time provided. The available format codes 
+        can be referenced in the datetime module documentation.
+    local_timezone: str
+        The local timezone. See the pytz documentation for available
+        timezones.
+    '''
+
+    # create datetime object out of provided date
+    date = datetime.strptime(date, date_format)
+
+    # create datetime object out of provided time
+    time = datetime.strptime(time, time_format)
+
+    # combine date and time to create datetime object with date and time
+    dt = datetime.combine(date.date(), time.time())
+
+    if convert_to_utc:
+        # get the local timezone
+        local_timezone = pytz.timezone(local_timezone)
+
+        # convert the local time to UTC
+        dt = local_timezone.localize(dt).astimezone(pytz.utc)
+
+    # convert to correct format
+    dt = dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    return dt
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from _utils import read_config, validate_exchange, get_base_url, get_candlestick_url
+from _utils import read_config, validate_exchange, get_base_url, get_candlestick_url, get_datetime
 import requests
 import pandas as pd
 from datetime import datetime, timedelta
+import pytz
 
 
 def get_valid_exchanges() -> list:
@@ -94,6 +95,7 @@ class Trades:
         start_time: str = '00:00:00',
         end_time: str = '23:59:59',
         time_format: str = '%H:%M:%S',
+        local_timezone: str = 'America/New_York',
         time_interval: str = '1d') -> pd.DataFrame:
         '''
         A method that returns a pandas dataframe with candlestick data from the exchange.
@@ -124,6 +126,9 @@ class Trades:
             the record with the most recent candlestick data will be equal to the
             current candle. Like end_date, different time formats can be provided 
             by changing the time_format parameter.
+        local_timezone: str
+            The local timezone. This will be needed when working with any time interval less
+            than 1d. See the pytz documentation for available timezones.
         time_interval: str
             The time interval for the trading data. Valid arguments include:
             '1m', '5m', '15m', '1h', '6h', '1d'. Default is 1d.
@@ -142,26 +147,19 @@ class Trades:
         2	2022-10-03	18958.29	19717.67	19059.10	19633.46	28571.640187
         '''
 
-        #TODO need to provide support for indicators. It will ideally get added to this function since we want to pull the data once
         if not isinstance(ticker_id, str):
             raise TypeError('ticker_id must be a string')
 
         ticker_id = ticker_id.upper()
 
         valid_ids = get_all_tickers(self.exchange)['id'].values
+        
         if ticker_id not in valid_ids:
             raise ValueError('Please provide a valid ticker id. Run get_all_tickers() for a full list of tickers')
 
         self.ticker_id = ticker_id
 
-        #TODO need to see if I can update this so that the format is predicted versus having to explicitly state format.
-        start_date = datetime.strptime(start_date, date_format).strftime('%Y-%m-%d')
-        start_time = datetime.strptime(start_time, time_format).strftime('%H:%M:%S')
-        start_datetime = datetime.strptime(start_date + ' ' + start_time, '%Y-%m-%d %H:%M:%S')
-        #TODO need tell datetime object that times are in utc, then need to convert to users local time
-        end_date = datetime.strptime(end_date, date_format).strftime('%Y-%m-%d')
-        end_time = datetime.strptime(end_time, time_format).strftime('%H:%M:%S')
-        end_datetime = datetime.strptime(end_date + ' ' + end_time, '%Y-%m-%d %H:%M:%S')
+        #TODO somehow add logic to handle when someone provides a date way far out or start date super far back
 
         time_interval_to_seconds = {
             '1m': 60,
@@ -173,6 +171,7 @@ class Trades:
         }
 
         #TODO will need to run time delta calcs to see if num records will be greater than 300, if so, then need to run loop.
+        #TODO need to add warning for user if they aren't pulling expected start date, example, if your interval is daily and start time if 01:00:00, its not going to pull the start date provided
 
         if time_interval not in time_interval_to_seconds.keys():
             msg = (
@@ -180,14 +179,46 @@ class Trades:
                 "Valid intervals are: "
             )
             raise KeyError(msg + str(list(time_interval_to_seconds.keys())))
+        
+
+        if time_interval == '1d':
+
+            start_datetime = get_datetime(date=start_date,
+                                          time=start_time,
+                                          date_format=date_format,
+                                          time_format=time_format)
+             
+            end_datetime = get_datetime(date=end_date,
+                                        time=end_time,
+                                        date_format=date_format,
+                                        time_format=time_format)
+        
+        else:
+            
+            start_datetime = get_datetime(date=start_date,
+                                          time=start_time,
+                                          date_format=date_format,
+                                          time_format=time_format,
+                                          convert_to_utc=True,
+                                          local_timezone=local_timezone)
+             
+            end_datetime = get_datetime(date=end_date,
+                                        time=end_time,
+                                        date_format=date_format,
+                                        time_format=time_format,
+                                        convert_to_utc=True,
+                                        local_timezone=local_timezone)
+            
 
         url = get_candlestick_url(self.exchange)
         url = url.format(ticker_id=self.ticker_id)
         
+        #TODO add capability to pull more than 300 candles but no more than 10K - if over 10k, raise warning saying to chunk it up.
+
         payload = {
             'granularity': time_interval_to_seconds[time_interval],
-            'start': start_datetime.strftime('%Y-%m-%d %H:%M:%S') ,
-            'end': end_datetime.strftime('%Y-%m-%d %H:%M:%S')
+            'start': start_datetime,
+            'end': end_datetime
             }
 
         response = requests.get(url, params=payload)
@@ -195,15 +226,19 @@ class Trades:
         if response.status_code != 200:
             raise Exception(f'Failed to retrieve data from {self.exchange} api')
 
-        df = pd.DataFrame(data=response.json(), columns=['time', 'low', 'high', 'open', 'close', 'volume'])
+        self.df = pd.DataFrame(data=response.json(), columns=['time', 'low', 'high', 'open', 'close', 'volume'])
 
         # converting unix time to datetime
-        df['time'] = pd.to_datetime(df['time'], unit='s')
+        self.df['time'] = pd.to_datetime(self.df['time'], unit='s', utc=True)
+
+        if time_interval != '1d':
+            # converting utc to local_timezone
+            self.df['time'] = self.df['time'].dt.tz_convert(pytz.timezone(local_timezone))
 
         # sorting by time
-        df.sort_values(by=['time'], inplace=True, ignore_index=True)
+        self.df.sort_values(by=['time'], inplace=True, ignore_index=True)
 
-        return df
+        return self.df
 
 
         
